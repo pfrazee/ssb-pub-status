2b2428cee7c600e79000c7ea7818babdfdc7b02d
Paul Frazee <pfrazee@gmail.com>
2014-12-30 19:04:17 -0600

    basic ui

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var ssbKeys  = require('ssb-keys')
 var path     = require('path')
 var explain  = require('explain-error')
 var manifest = require('scuttlebot/lib/manifest')
+var http     = require('http')
 var config   = require('./config')
 
 var keys = ssbKeys.loadOrCreateSync(path.join(config.path, 'secret'))
 var rpc = require('scuttlebot/client')(config.sbot_address, manifest, function (err) {
   if(err) throw err
 })
+var msgs = []
 
 rpc.auth(ssbKeys.signObj(keys, {
   role: 'client',
@@ -20,8 +22,19 @@ rpc.auth(ssbKeys.signObj(keys, {
     else
       throw explain(err, 'auth failed')
   }
-  rpc.whoami(function(err, data) {
-    console.log(err, data)
-    process.exit()
-  })
+
+  http.createServer(onRequest).listen(config.port)
+  console.log('listening on', config.port)
 })
+
+var banner = [
+  '<strong>Hello world, I am '+keys.id+'</strong>',
+  'this pub server is running <a href="https://github.com/ssbc/scuttlebot">scuttlebot</a> on the secure-scuttlebutt network',
+].join('\n')
+msgs.push('<small>12/30 5:15pm</small>\nThings are going great here. How are you?')
+
+function onRequest(req, res) {
+  res.setHeader('Content-Type', 'text/html')
+  res.writeHead(200)
+  res.end('<style>body { font-family: monospace; white-space: pre }</style>'+[banner].concat(msgs).join('\n\n'))
+}
