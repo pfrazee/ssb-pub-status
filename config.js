67ed48f319a7f86ad14ad38702af669fd9196909
Paul Frazee <pfrazee@gmail.com>
2014-12-30 19:53:32 -0600

    rename rc to pubstatus

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var path = require('path')
 
 var nonPrivate = require('non-private-ip')
 
-module.exports = require('rc')('ssb', {
+module.exports = require('rc')('pubstatus', {
   hostname: nonPrivate() || '',
   port: 8000,
   path: path.join(process.env.HOME, '.ssb'),
