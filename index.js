6c3cbbd06fdae3bbe4ebb6f67bde28b61d49c3bf
Paul Frazee <pfrazee@gmail.com>
2014-12-30 19:41:07 -0600

    added header ascii art

--- index.js
+++ index.js
@@ -37,11 +37,30 @@ rpc.auth(ssbKeys.signObj(keys, {
 })
 
 var banner = [
+  '             .,ad88888888baa,',
+  '        ,d8P"""        ""9888ba.',
+  '     .a8"          ,ad88888888888a',
+  '    aP\'          ,88888888888888888a',
+  '  ,8"           ,88888888888888888888,',
+  ' ,8\'            (888888888( )888888888,',
+  ',8\'             `8888888888888888888888',
+  '8)               `888888888888888888888,',
+  '8                  "8888888888888888888)',
+  '8                   `888888888888888888)',
+  '8)                    "8888888888888888',
+  '(b                     "88888888888888\'',
+  '`8,        (8)          8888888888888)',
+  ' "8a                   ,888888888888)',
+  '   V8,                 d88888888888"',
+  '    `8b,             ,d8888888888P\'',
+  '      `V8a,       ,ad8888888888P\'',
+  '         ""8888888888888888P"',
+  '',
   '<strong>Hello world, I am '+keys.id+'</strong>',
   'this pub server is running <a href="https://github.com/ssbc/scuttlebot">scuttlebot</a> on the secure-scuttlebutt network',
   '',
   'latest updates:'
-].join('\n')
+].join('\n').replace(/ /g, '&nbsp;')
 
 function onRequest(req, res) {
   res.setHeader('Content-Type', 'text/html')
